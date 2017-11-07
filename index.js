246cabde8a0f517c1631bd031f319abd760517b3
Tom <tomnik104@yahoo.de>
2017-11-07 13:47:55 +0100

    v 4.0 / Fixes #1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var xmrig = angular.module('xmrigstats', ['ngMaterial']);
-xmrig.controller('MainController', function ($scope, $http, $mdDialog) {
-    var x;
+var x;
+var interval;
+xmrig.controller('MainController', function ($scope, $http, $mdDialog, $mdSidenav) {
     $scope.safeApply = function (fn) {
         var phase = this.$root.$$phase;
         if (phase === '$apply' || phase === '$digest') {
@@ -14,61 +15,112 @@ xmrig.controller('MainController', function ($scope, $http, $mdDialog) {
         }
     };
 
-    $scope.update = function () {
-        $http.get(localStorage.getItem('url') + ':' + localStorage.getItem('port'), {
-            headers: {
-                'Authorization': 'Bearer ' + localStorage.getItem('auth')
-            }
-        }).then(function (data) {
-            var d = new Date();
-            d.setTime(d.getTime() - data.data.connection.uptime * 1000);
-            $scope.res = data.data;
-            $scope.res.started = getDate(d);
-        }).catch(function (err) {
-            console.log(err);
-            $mdDialog.show($mdDialog.alert().clickOutsideToClose(true).title('Error').textContent('We could not reach a Miner at ' + err.config.url + '. Sleeping for 60 Seconds!').ok('Damn!'));
-            clearInterval(x);
-            interval(60*1000);
-        });
-    };
-    $scope.update();
-    interval();
-    function interval(value) {
-        console.log('Updating: ' + localStorage.getItem('update')*1000);
-        x = setInterval(function () {
-            $scope.update();
-        }, value || localStorage.getItem('update')*1000 || 5000);
+    $scope.rigs = JSON.parse(localStorage.getItem('rigs')) || [];
+    $scope.currentRig = 0;
+    if(JSON.parse(localStorage.getItem('rigs'))){
+        $scope.type = JSON.parse(localStorage.getItem('rigs'))[$scope.currentRig];
     }
+
+    $scope.toggleMenu = function () {
+        $mdSidenav('menu').toggle();
+    };
+
+    function update() {
+        try {
+            var json = ($scope.rigs[$scope.currentRig]);
+            if(json){
+                $http.get(json.url + ':' + json.port).then(function (data) {
+                    if(json.type === "proxy"){
+                        $http.get(json.url + ':' + json.port + '/workers.json').then(function (w) {
+                            for(var i = 0; i < w.data.workers.length; i++){
+                                w.data.workers[i][7] = $scope.date(new Date(w.data.workers[i][7]));
+                            }
+                            $scope.workers = w.data.workers;
+                        });
+                        data.data.uptime = $scope.date(new Date(new Date() - data.data.uptime));
+                    }
+                    $scope.res = data.data;
+                });
+            }else {
+                throw new Error('No Miner saved')
+            }
+        }catch (e){
+            console.error(e);
+            $mdDialog.show($mdDialog.alert().clickOutsideToClose(true).title('Error').textContent('We could not reach the Miner and encountered an Error: ' + e.message).ok('Damn!'));
+        }
+    }
+
+    update();
+
     $scope.settings = function (ev) {
         $mdDialog.show({
             controller: SettingsController,
-            templateUrl: 'tmpl/settings.html',
+            templateUrl: 'tmpl/add.html',
             parent: angular.element(document.body),
             targetEvent: ev,
             clickOutsideToClose: true,
-            fullscreen: false
-        });
+            fullscreen: true
+        }).then( function () {
+            $scope.rigs = JSON.parse(localStorage.getItem('rigs'));
+        })
     };
 
-    function SettingsController($scope, $mdDialog) {
-        $scope.rig = localStorage;
-        $scope.cancel = function () {
-            $mdDialog.hide();
-        };
-        $scope.save = function () {
-            localStorage.setItem('url', $scope.rig.url || null);
-            localStorage.setItem('port', $scope.rig.port || null);
-            localStorage.setItem('auth', $scope.rig.auth || null);
-            localStorage.setItem('update', $scope.rig.update || 5);
-            clearInterval(x);
-            interval();
-            $mdDialog.hide();
-        };
-    }
+    $scope.remove = function (ev) {
+        $mdDialog.show({
+            controller: RemoveController,
+            templateUrl: 'tmpl/remove.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            fullscreen: true
+        }).then( function () {
+            $scope.rigs = JSON.parse(localStorage.getItem('rigs'));
+        })
+    };
+    
+    $scope.close = function (number) {
+        console.log('Changing to ' + $scope.rigs[number].name);
+        $scope.currentRig = number;
+        $scope.type = JSON.parse(localStorage.getItem('rigs'))[$scope.currentRig].type;
+        console.log($scope.type);
+        update();
+        $mdSidenav('menu').close();
+    };
 
-    function getDate(d) {
+    $scope.date = function (d) {
         return ('0' + d.getDate()).toString().substr(-2) + '.' + ('0' + (d.getMonth() + 1)).toString().substr(-2) + '.' + d.getFullYear() + ' / ' + ('0' + d.getHours()).toString().substr(-2) + ':' + ('0' + d.getMinutes()).toString().substr(-2) + ':' + ('0' + d.getSeconds()).toString().substr(-2);
     }
+
 }).config(function ($mdThemingProvider) {
     $mdThemingProvider.theme('default').primaryPalette('orange').accentPalette('pink').dark();
 });
+
+function SettingsController($scope, $mdDialog) {
+    $scope.cancel = function () {
+        $mdDialog.hide();
+    };
+    $scope.save = function () {
+        var current = JSON.parse(localStorage.getItem('rigs')) || [];
+        current.push({
+            url: $scope.rig.url,
+            type: $scope.rig.type,
+            port: $scope.rig.port,
+            auth: $scope.rig.auth,
+            name: $scope.rig.name
+        });
+        localStorage.setItem('rigs', JSON.stringify(current));
+        $mdDialog.hide();
+    };
+}
+
+function RemoveController($scope, $mdDialog) {
+    $scope.rigs =  JSON.parse(localStorage.getItem('rigs')) || [];
+    $scope.cancel = function () {
+        $mdDialog.hide();
+    };
+    $scope.removeRig = function (i) {
+        console.log(JSON.stringify($scope.rigs.splice(i, 1)));
+        localStorage.clear();
+        localStorage.setItem('rigs', JSON.stringify($scope.rigs.splice(i, 1)))
+    }
+}
